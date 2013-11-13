ebb10427ac8a898c0a548eb4e0c56ee10a088988
Katherine Whitlock <toroidalcode@gmail.com>
2013-11-13 01:31:07 -0500

    Optimized Rust example

--- rust/prob1.rs
+++ rust/prob1.rs
@@ -1,10 +1,9 @@
 fn main() {
-	let sum = range(0, 1000).fold(0, |sum, x| { 
-		if x % 3 == 0 || x % 5 == 0 {
-			sum + x
-		} else {
-			sum
-		}
-	});
-	println!("The sum of all numbers up to 1000 divisible by 3 or 5: {:i}", sum)
+    let sum = range(0, 1000).filter( |x| { 
+        x % 3 == 0 || x % 5 == 0
+    }).fold( 0, |sum, tail| {
+        sum + tail
+    });
+    
+    println!("The sum of all numbers up to 1000 divisible by 3 or 5: {:i}", sum);
 }
